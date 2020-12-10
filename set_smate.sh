2fc1782e123035c5e8dec08cefac9002a8cd197d
joaquinabian <gatoygata2@gmail.com>
2020-12-10 13:49:47 +0100

    set main text witdh

--- set_smate.sh
+++ set_smate.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
 # 
-##########################   SET DESKTOP FOLDERS FOR INDI & KSTARS LOGS    #########################
+######################  SET DESKTOP FOLDERS FOR INDI & KSTARS LOGS  ############
 # Make two soft links on the desktop to Logs folders
 echo "create links to logs"
 ln -s /home/stellarmate/.indi/logs /home/stellarmate/Desktop/indi_logs
 ln -s /home/stellarmate/.local/share/kstars/logs /home/stellarmate/Desktop/kstars_logs
 echo "done"
 #
-################################  REMOVE NOT USED FOLDERS   ########################################
+############################  REMOVE NOT USED FOLDERS   ########################
 # I use 'rmdir' instead of 'rm -r' because if there is something inside it will fail
 # remove subfolders first, then main folder 
 echo "remove .astropy folder"
@@ -19,7 +19,7 @@ echo "done"
 echo "remove astropy link"
 rm /home/stellarmate/.config/astropy || echo "astropy link cannot be removed"
 echo "done"
-################################   CHANGE NAME OF MACHINE    #######################################
+###########################  CHANGE NAME OF MACHINE  ###########################
 # to master, slave o test:
 #     stellarmate@master
 # sudo nano /etc/hosts
@@ -29,11 +29,11 @@ echo "done"
 #
 echo "change raspi name to stellarmate@$RPITYPE"
 echo "to be done"
-############################   DONT START KSTARS AT BOOT  ##########################################
+#########################   DONT START KSTARS AT BOOT  #########################
 echo "Do not start KStars at boot"
 TARGET="/home/stellarmate/.config/autostart/KStars.desktop"
 text="NotShowIn=LXDE;"
 echo $text >> $TARGET
 echo "done"
-####################################################################################################
+################################################################################
 
