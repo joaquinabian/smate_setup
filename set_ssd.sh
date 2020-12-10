048858c3c47f3783ef0de95c0f66c25ed2870239
joaquinabian <gatoygata2@gmail.com>
2020-12-10 13:49:25 +0100

    shorten echo sentence

--- set_ssd.sh
+++ set_ssd.sh
@@ -15,7 +15,7 @@ sed -i "s/^\#\?.\?FIRMWARE_RELEASE_STATUS=.*$/FIRMWARE_RELEASE_STATUS=\"stable\"
 sudo rpi-eeprom-update -d -a
 sync
 echo "Please Reboot to activate changes"
-echo "After reboot check that <vcgencmd bootloader_version> returns version older than June 15, 2020"
+echo "Check that <vcgencmd bootloader_version> returns version older than June 15, 2020"
 echo "Then copy SD card to SSD  with 'SD card copier'"
 echo "Then shutdown and remove SD card"
 
